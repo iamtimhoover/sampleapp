53f9833944e545ff5e53232b5b9a66e9bed041e8
Tim Hoover <timothyhoov@gmail.com>
2013-09-01 15:28:45 -0400

    finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,16 @@
 class UsersController < ApplicationController
+
+	before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+	before_action :correct_user, only: [:edit, :update]
+
+	def index
+		@users = User.paginate(page: params[:page])
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
   def new
   	@user = User.new
   end
@@ -15,6 +27,26 @@ class UsersController < ApplicationController
   	end
   end
 
+  def edit
+  end
+
+  def update
+  	@user = User.find(params[:id])
+  	if @user.update_attributes(user_params)
+  		flash[:success] = "Profile updated"
+  		sign_in @user
+		redirect_to @user
+  	else
+  		render 'edit'
+  	end
+  end
+
+  def destroy
+    User.find_by(params[:id])
+    flash[:success] = "User destroyed"
+    redirect_to users_url
+  end
+
   def show
   	@user = User.find(params[:id])
   end
@@ -22,7 +54,37 @@ class UsersController < ApplicationController
   private
 
   def user_params
-  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  	params.require(:user).permit(:name, :email, :password, :password_confirmation, :admin)
   end
 
+  # before filters
+
+  def signed_in_user
+  	redirect_to signin_url, notice: "Please sign in." unless signed_in?
+  end
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
+
+
 end
+
+
+
+
+
+
+
